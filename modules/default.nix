8419bb254fc92f7522f620af70aebf6f0dea1153
Omar Fawzy <anaomarfawzy@gmail.com>
2025-09-26 21:55:04 +0300

    temporary disable hypr conf

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,11 @@
-self: anyrun: illogical-impulse-dotfiles: inputs: {... }:
-{
+self: anyrun: illogical-impulse-dotfiles: inputs:
+{ ... }: {
   imports = [
     (import ./options.nix illogical-impulse-dotfiles)
     (import ./anyrun.nix anyrun illogical-impulse-dotfiles inputs)
     (import ./quickshell.nix illogical-impulse-dotfiles inputs)
-    (import ./hyprland.nix illogical-impulse-dotfiles inputs)
+    #(import ./hyprland.nix illogical-impulse-dotfiles inputs)
     (import ./packages.nix inputs)
   ];
 }
+
